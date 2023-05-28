1b0c27fd93d57e43a7760971caae83cf2ac8ffb0
Hrechanyuk Anastasiia <anastasiya.grk@gmail.com>
2023-05-28 21:53:06 +0300

    1 завдання

--- 28.05.py
+++ 28.05.py
@@ -1,24 +1,19 @@
 #1
 class OddIterator:
-    def __init__(self, n):
-        if n < 1:
-            raise ValueError("Введіть додатне число")
-        self.n = n
-        self.current = 1
-
+    def __init__(self, name):
+        self.name = name
+        self.index = 0
     def __iter__(self):
         return self
-
     def __next__(self):
-        if self.current > self.n:
+        if self.index >= len(self.name):
             raise StopIteration
-        else:
-            result = self.current
-            self.current += 2
-            return result
-#приклад використання
-try:
-    for num in OddIterator(10):
-        print(num)
-except ValueError as e:
-    print(e)
+        if self.index < 0:
+            raise ValueError("Помилка!")
+        value = self.name[self.index]
+        self.index += 2
+        return value
+my_list = [1,2,3,4,5,6,7,8,9,10]
+my_iter = OddIterator(my_list)
+for num in my_iter:
+    print(num)
