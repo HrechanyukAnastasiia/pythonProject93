1aa85ad9c8689ec5205bbd080a470e8a8b2db759
Hrechanyuk Anastasiia <anastasiya.grk@gmail.com>
2023-05-28 21:49:04 +0300

    2 завдання

---
+++ 28.05.py
@@ -0,0 +1,14 @@
+#2
+my_list = [1,2,3,4,5,6,7,8,9,10]
+class SquareGenerator:
+    def __init__(self):
+        if isinstance(my_list, list) is True:
+            print(my_list)
+        elif isinstance(my_list, list) is not True:
+            TypeError("Помилка!")
+    def my_generator(self, name):
+        for num in my_list:
+            num = num ** 2
+            yield num
+    for num in my_generator("", my_list):
+        print(num)
